1c8e7c7c164ae947683752394cd90de0a0d3805a
ZengMing <zengm@fenqi.im>
2018-10-27 23:14:16 +0800

    fix bugs

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/handler/demo_handler.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/handler/demo_handler.py
@@ -4,17 +4,19 @@
 """
 
 from flask_restful import current_app
+from extensions import db
 from {{cookiecutter.app_name}}.models.models import DemoModel
 
 
-def query_by_id(id):
+def query_by_id(d_id):
     result = None
-    if id:
+    if d_id:
         my_app = current_app
         with my_app.app_context() as appc:
-            demo_obj = DemoModel.query.filter_by(id=id).first()
+            demo_obj = DemoModel.query.filter_by(id=d_id).first()
             if demo_obj:
                 result = demo_obj.serialize()
+                my_app.logger.info(r'search success')
             db.session.close()
     return result
 
