042222cc3c52177ed766da1ac783e705a30ef17d
zengmingcd <zengmingcd@163.com>
2018-11-13 11:41:00 +0800

    Update env_init.sh
    fix spell bug

--- {{cookiecutter.project_name}}/env_init.sh
+++ {{cookiecutter.project_name}}/env_init.sh
@@ -6,7 +6,7 @@ cd ..
 mkdir venv
 cd venv
 virtualenv -p python3 --no-site-packages ./{{cookiecutter.project_name}}
-source {{cookiecutter.project_name}}/bin/activite
+source {{cookiecutter.project_name}}/bin/activate
 cd ${code_dir}
 pip3 install -r requirements.txt
 echo 初始化完毕.
