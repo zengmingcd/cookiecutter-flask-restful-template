e33fee480695b76679ae9adc79cfdf497bd5d9f1
ZengMing <zengm@fenqi.im>
2018-11-24 00:00:48 +0800

    fix shell bug

--- {{cookiecutter.project_name}}/env_init.sh
+++ {{cookiecutter.project_name}}/env_init.sh
@@ -6,7 +6,7 @@ cd ..
 mkdir venv
 cd venv
 virtualenv -p python3 --no-site-packages ./{{cookiecutter.project_name}}
-source {{cookiecutter.project_name}}/bin/activate
+source {{cookiecutter.project_name}}/bin/activate || { echo "source error"; exit 1; }
 cd ${code_dir}
 pip3 install -r requirements.txt
 echo 初始化完毕.
